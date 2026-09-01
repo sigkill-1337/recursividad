9e72d36394422d2c39c2221379efd1356ceaba3c
cherry <guillermo55667@email.com>
2026-08-31 19:34:11 -0500

    grr

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static int conteo = 0;
+
+    static void contar(int numero) {
+
+
+        if (numero == 0) {
+            System.out.println("Fin");
+            System.out.println("Intentos: " + conteo);
+            return;
+        }
+    
+
+        System.out.println(numero);
+        conteo++;
+
+        contar(numero - 1);
+} 
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero positivo: ");
+        int numero = scanner.nextInt();
+        contar(numero);
+        scanner.close();
     }
 }
